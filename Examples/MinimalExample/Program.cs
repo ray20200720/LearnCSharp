30e674fb9d97b11458ee420316992ce421b27ca3
Ray Kuo <ray20200720@gmail.com>
2023-12-29 17:27:50 +0800

    Create Minimal Example

---
+++ Examples/MinimalExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MinimalExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            // https://learn.microsoft.com/zh-tw/dotnet/csharp/tour-of-csharp/
+
+            Console.WriteLine("Hello, World");
+        }
+    }
+}
