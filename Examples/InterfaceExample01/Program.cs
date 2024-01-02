ac459bc052b7638c21b64d2043babfb25526f209
Ray Kuo <ray20200720@gmail.com>
2024-01-02 16:41:30 +0800

    Create InterfaceExample01

---
+++ Examples/InterfaceExample01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExample01
+{
+    class Program
+    {
+        static void PrintPoint(IPoint p)
+        {
+            Console.WriteLine("x={0}, y={1}", p.X, p.Y);
+        }
+
+        static void Main(string[] args)
+        {
+            //https://learn.microsoft.com/zh-tw/dotnet/csharp/language-reference/keywords/interface
+            IPoint p = new Point(3, 4);
+            Console.Write("My Point: ");
+            PrintPoint(p);
+            Console.WriteLine("My Point Distance: {0}", p.Distance);
+
+            Console.ReadKey();
+        }
+    }
+}
