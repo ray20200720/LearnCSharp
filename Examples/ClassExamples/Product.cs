75a49a6d49e79e395710e2a9698009783a3927e4
Ray Kuo <ray20200720@gmail.com>
2024-01-03 17:20:42 +0800

    create ClassExamples

---
+++ Examples/ClassExamples/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExamples
+{
+    class Product
+    {
+        private string name; // field
+        public string Name // property and read-only
+        { 
+            get { return name; }
+            //set { name = value; }
+        }
+
+        private double price;
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    price = 0;
+                    throw new Exception("price does not allow be negative");
+                }
+                else
+                {
+                    price = value;
+                }
+            }
+        }
+        
+        public Product(string initialName)
+        {
+            name = initialName;
+        }
+    }
+}
---
+++ Examples/ClassExamples/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Product macAir = new Product("Mac Air");
+                macAir.Price = 10;
+                //macAir.Name = "I'm not Mac Air"; // error, Name is readonly
+                Console.WriteLine("{0} {1}", macAir.Name, macAir.Price);
+
+                Product mac = new Product("Mac");
+                mac.Price = 20;
+                Console.WriteLine("{0} {1}", mac.Name, mac.Price);
+
+                Product iPhone = new Product("iPhone");
+                iPhone.Price = -99;
+                Console.WriteLine("{0} {1}", iPhone.Name, iPhone.Price);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
