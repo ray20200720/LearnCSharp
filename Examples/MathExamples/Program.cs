4431b763bf0b2b0073897f0662c7f2e2a68f85f0
Ray Kuo <ray20200720@gmail.com>
2024-01-03 11:09:30 +0800

    create MathExamples

---
+++ Examples/MathExamples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5, num2 = 10;
+
+            Console.WriteLine("{0} and {1}, Max is: {2} ", num1, num2, Math.Max(num1, num2));
+            Console.WriteLine("{0} and {1}, Min is: {2} ", num1, num2, Math.Min(num1, num2));
+
+            var num = -4;
+            Console.WriteLine("The absolute value of {0} is: {1}", num, Math.Abs(num));
+
+            num = Math.Abs(num);
+            Console.WriteLine("The square root of {0} is: {1}", num, Math.Sqrt(num));
+
+
+            Console.ReadKey();
+        }
+    }
+}
