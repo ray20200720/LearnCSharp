9e7fe40d251716c98147f53018c311ebd537b415
Ray Kuo <ray20200720@gmail.com>
2024-01-03 10:47:21 +0800

    Create InputExamples

---
+++ Examples/InputExamples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("Hi, {0}", yourName);
+
+            Console.WriteLine("How old are you?");
+
+            int yourAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your age is: {0}", yourAge);
+
+            Console.ReadKey();
+        }
+    }
+}
