76905a3e6529e3439571bedc987b8f6d06fb0f34
Ray Kuo <ray20200720@gmail.com>
2024-01-03 10:41:20 +0800

    Create ConstExamples

---
+++ Examples/ConstExamples/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int num = 1;
+            //num = 2; // error
+
+            Console.WriteLine("{0}", num);
+
+            Console.ReadKey();
+        }
+    }
+}
