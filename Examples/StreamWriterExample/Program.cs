c1053f264aa86ece2ba1142c296d6d66b2a0ac2d
Ray Kuo <ray20200720@gmail.com>
2024-01-16 16:44:10 +0800

    create StreamWriterExample

---
+++ Examples/StreamWriterExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamWriterExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a string array with the lines of text
+            string[] lines = { "First line", "Second line", "Third line" };
+
+            // Set a variable to the Documents path.
+            string docPath =
+              //Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+              Directory.GetCurrentDirectory();
+
+            // Write the string array to a new file named "WriteLines.txt".
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.txt")))
+            {
+                foreach (string line in lines)
+                    outputFile.WriteLine(line);
+            }
+        }
+    }
+}
