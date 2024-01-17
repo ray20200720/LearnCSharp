5029d78b76430689233a671b00cc11472af7a1e0
Ray Kuo <ray20200720@gmail.com>
2024-01-17 17:08:13 +0800

    create EnumExample

--- Examples/ConstExamples/Program.cs
+++ Examples/ConstExamples/Program.cs
@@ -6,6 +6,17 @@ using System.Threading.Tasks;
 
 namespace ConstExamples
 {
+    public class Constants
+    {
+        public const double PI = 3.1416;
+
+        public class Letter
+        {
+            public const char A = 'A';
+            public const char B = 'B';
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -14,6 +25,10 @@ namespace ConstExamples
             //num = 2; // error
 
             Console.WriteLine("{0}", num);
+
+            Console.WriteLine("{0}", Constants.PI);
+
+            Console.WriteLine("{0}", Constants.Letter.A);
 
             Console.ReadKey();
         }
---
+++ Examples/EnumExample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExample
+{
+    public enum ComputerType
+    {
+        Desktop = 0,
+        Laptop = 1,
+        Server = 2
+    }
+
+    class Program
+    {
+        static string GetComputerType(ComputerType type)
+        {
+            return string.Format("{0}", Enum.GetName(type.GetType(), type));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===============================");
+
+            ComputerType iWant = ComputerType.Laptop;
+
+            Console.WriteLine("{0}", iWant);
+            Console.WriteLine("{0}", Enum.GetName(iWant.GetType(), iWant));
+            
+            Console.WriteLine("===============================");
+
+            ComputerType youWant = ComputerType.Server;
+
+            Console.WriteLine("{0}", youWant.GetType());
+            Console.WriteLine("{0}", Enum.GetName(youWant.GetType(), youWant));
+            Console.WriteLine("{0}", GetComputerType(youWant));
+
+            Console.WriteLine("===============================");
+
+            var someType = ComputerType.Desktop;
+            var values = Enum.GetValues(someType.GetType());
+            foreach(var value in values)
+            {
+                Console.WriteLine("{0}", value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
