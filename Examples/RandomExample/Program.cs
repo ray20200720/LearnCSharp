136b207c957bcffe573670b78306c3e0c8dc6d6b
Ray Kuo <ray20200720@gmail.com>
2024-01-22 16:12:49 +0800

    create RandomExample

---
+++ Examples/RandomExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            
+            int randomNumber = random.Next(); // 傳回非負值隨的機整數
+            Console.WriteLine("randomNumber is {0}", randomNumber);
+
+            randomNumber = random.Next(100); // 傳回小於指定之最大值的非負值隨機整數
+            Console.WriteLine("randomNumber is {0}", randomNumber);
+
+            randomNumber = random.Next(1, 6); // 傳回指定範圍內的隨機整數
+            Console.WriteLine("randomNumber is {0}", randomNumber);
+
+            Console.ReadKey();
+
+        }
+    }
+}
