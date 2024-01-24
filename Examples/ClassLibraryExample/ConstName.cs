c29ed7a8109bdecbc2b8f546d8dacca1e8333e7e
Ray Kuo <ray20200720@gmail.com>
2024-01-24 16:20:05 +0800

    create ClassLibraryExample/ConstName

---
+++ Examples/ClassLibraryExample/ConstName.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryExample
+{
+    class ConstName
+    {
+        public const char AndChar = '&';
+        public const char AtChar = '@';
+        public const char BarChar = '|';
+        public const char ColonChar = ':';
+        public const char CommaChar = ',';
+        public const char DashChar = '-';
+        public const char DotChar = '.';
+        public const char HashChar = '#';
+        public const char SemiColonChar = ';';
+        public const char SlashChar = '/';
+        public const char StarChar = '*';
+        public const char SpaceChar = ' ';
+        public const char TildeChar = '~';
+        public const char UnderScoreChar = '_';
+
+        public const string AndStr = "&";
+        public const string ArrowStr = "->";
+        public const string AtStr = "@";
+        public const string BarStr = "|";
+        public const string ColonStr = ":";
+        public const string CommaStr = ",";
+        public const string DashStr = "-";
+        public const string DotStr = ".";
+        public const string HashStr = "#";
+        public const string SemiColonStr = ";";
+        public const string SlashStr = "/";
+        public const string StarStr = "*";
+        public const string SpaceStr = " ";
+        public const string TildeStr = "~";
+        public const string UnderScoreStr = "_";
+
+        class Letter
+        {
+            public const string Y = "Y";
+            public const string N = "N";
+        }
+
+        class DateFormat
+        {
+            public const string DateTimeFormat = "yyyyMMdd-HHmmss";
+            public const string MM = "MM";
+            public const string MMdd = "MMdd";
+            public const string SqlDateFormat = "yyyy-MM-dd";
+            public const string SqlDateTimeFormat = "yyyy-MM-dd HH:mm:ss";
+            public const string TxnIdFormat = "yyyyMMddHHmmssfff";
+            public const string YM = "YM";
+            public const string YYMMddHH = "yyMMddHH";
+            public const string yyMMddHHmmssffff = "yyMMddHHmmssffff";
+            public const string yyyy = "yyyy";
+            public const string yyyyMMdd = "yyyyMMdd";
+            public const string yyyyMMddHHmmss = "yyyyMMddHHmmss";
+            public const string yyyyMMddHHmmssfff = "yyyyMMddHHmmss.fff";
+        }
+    }
+}
