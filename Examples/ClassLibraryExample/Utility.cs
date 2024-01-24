efadd703df5a6ea3b7a2b287ba156d3ae567a963
Ray Kuo <ray20200720@gmail.com>
2024-01-24 14:49:32 +0800

    create ClassLibraryExample

---
+++ Examples/ClassLibraryExample/Utility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryExample
+{
+    public class Utility
+    {
+        public static void PrintDelimiter(string title = "", int length = 50, string separator = "=")
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (i == (length - 1))
+                {
+                    Console.WriteLine("{0}", separator);
+                }
+                else if(i == (length / 2))
+                {
+                    Console.Write("{0}", title);
+                }
+                else
+                {
+                    Console.Write("{0}", separator);
+                }
+            }
+        }
+    }
+}
