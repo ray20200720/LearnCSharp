f02b3d11846dcc38f72598b11bf467ad632e4290
Ray Kuo <ray20200720@gmail.com>
2024-01-29 10:18:21 +0800

    create WindowsServiceInstallExample

---
+++ Examples/WindowsServiceInstallExample/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace WindowsServiceInstallExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Path = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_ServiceName = new System.Windows.Forms.TextBox();
+            this.btnInstall = new System.Windows.Forms.Button();
+            this.btnUninstall = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "應用程式路徑:";
+            // 
+            // txt_Path
+            // 
+            this.txt_Path.Location = new System.Drawing.Point(180, 42);
+            this.txt_Path.Name = "txt_Path";
+            this.txt_Path.Size = new System.Drawing.Size(829, 29);
+            this.txt_Path.TabIndex = 1;
+            this.txt_Path.Text = "D:\\Ray\\MyProjects\\LearnCSharp\\Examples\\WindowsServiceExample\\bin\\Debug\\WindowsSer" +
+    "viceExample.exe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "服務名稱:";
+            // 
+            // txt_ServiceName
+            // 
+            this.txt_ServiceName.Location = new System.Drawing.Point(180, 94);
+            this.txt_ServiceName.Name = "txt_ServiceName";
+            this.txt_ServiceName.Size = new System.Drawing.Size(205, 29);
+            this.txt_ServiceName.TabIndex = 3;
+            this.txt_ServiceName.Text = "MyService";
+            // 
+            // btnInstall
+            // 
+            this.btnInstall.Location = new System.Drawing.Point(74, 165);
+            this.btnInstall.Name = "btnInstall";
+            this.btnInstall.Size = new System.Drawing.Size(82, 43);
+            this.btnInstall.TabIndex = 4;
+            this.btnInstall.Text = "安裝";
+            this.btnInstall.UseVisualStyleBackColor = true;
+            this.btnInstall.Click += new System.EventHandler(this.btnInstall_Click);
+            // 
+            // btnUninstall
+            // 
+            this.btnUninstall.Location = new System.Drawing.Point(180, 165);
+            this.btnUninstall.Name = "btnUninstall";
+            this.btnUninstall.Size = new System.Drawing.Size(82, 43);
+            this.btnUninstall.TabIndex = 5;
+            this.btnUninstall.Text = "卸載";
+            this.btnUninstall.UseVisualStyleBackColor = true;
+            this.btnUninstall.Click += new System.EventHandler(this.btnUninstall_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(287, 165);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(82, 43);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "啟動";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(393, 165);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(82, 43);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 304);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnUninstall);
+            this.Controls.Add(this.btnInstall);
+            this.Controls.Add(this.txt_ServiceName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_Path);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "基於C#實現WindowsService安裝卸載啟動停止";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Path;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_ServiceName;
+        private System.Windows.Forms.Button btnInstall;
+        private System.Windows.Forms.Button btnUninstall;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ Examples/WindowsServiceInstallExample/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsServiceInstallExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool IsServiceExisted(string serviceName)
+        {
+            ServiceController[] services = ServiceController.GetServices();
+            foreach(ServiceController sc in services)
+            {
+                if (sc.ServiceName.ToLower() == serviceName.ToLower())
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool InstallService(string serviceFilePath)
+        {
+            try
+            {
+                using(AssemblyInstaller installer = new AssemblyInstaller())
+                {
+                    installer.UseNewContext = true;
+                    installer.Path = serviceFilePath;
+                    IDictionary savedState = new Hashtable();
+                    installer.Install(savedState);
+                    installer.Commit(savedState);
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool UnInstallService(string serviceFilePath)
+        {
+            try
+            {
+                using (AssemblyInstaller installer = new AssemblyInstaller())
+                {
+                    installer.UseNewContext = true;
+                    installer.Path = serviceFilePath;
+                    installer.Uninstall(null);
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool StartService(string serviceName)
+        {
+            try
+            {
+                using (ServiceController controller = new ServiceController(serviceName))
+                {
+                    if(controller.Status == ServiceControllerStatus.Stopped)
+                    {
+                        controller.Start();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool StopService(string serviceName)
+        {
+            try
+            {
+                using (ServiceController controller = new ServiceController(serviceName))
+                {
+                    if (controller.Status == ServiceControllerStatus.Running)
+                    {
+                        controller.Stop();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private string ServiceName
+        {
+            get { return this.txt_ServiceName.Text.Trim(); }
+        }
+
+        private string ServicePath
+        {
+            get { return this.txt_Path.Text.Trim(); }
+        }
+
+        private void btnInstall_Click(object sender, EventArgs e)
+        {
+            if (this.IsServiceExisted(ServiceName))
+            {
+
+            }
+            if (this.InstallService(ServicePath))
+            {
+                MessageBox.Show("服務安裝成功", "服務安裝");
+            }
+            else
+            {
+                MessageBox.Show("服務安裝失敗", "服務安裝");
+            }
+        }
+
+        private void btnUninstall_Click(object sender, EventArgs e)
+        {
+            if (this.IsServiceExisted(ServiceName))
+            {
+                if (this.StopService(ServiceName) && this.UnInstallService(ServicePath))
+                {
+                    MessageBox.Show("服務卸載成功", "服務卸載");
+                }
+                else
+                {
+                    MessageBox.Show("服務卸載失敗", "服務卸載");
+                }
+            }
+            else
+            {
+                MessageBox.Show("服務卸載失敗: 服務不存在", "服務卸載");
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if(this.IsServiceExisted(ServiceName))
+            {
+                if (this.StartService(ServiceName))
+                {
+                    MessageBox.Show("服務啟動成功", "服務啟動");
+                }
+                else
+                {
+                    MessageBox.Show("服務啟動失敗", "服務啟動");
+                }
+            }
+            else
+            {
+                MessageBox.Show("服務啟動失敗: 服務不存在", "服務啟動");
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (this.IsServiceExisted(ServiceName))
+            {
+                if (this.StopService(ServiceName))
+                {
+                    MessageBox.Show("服務停止成功", "服務停止");
+                }
+                else
+                {
+                    MessageBox.Show("服務停止失敗", "服務停止");
+                }
+            }
+            else
+            {
+                MessageBox.Show("服務停止失敗: 服務不存在", "服務停止");
+            }
+
+        }
+    }
+}
