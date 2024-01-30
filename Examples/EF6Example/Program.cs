a394ea4104ec60b114dd66de038c80cdbee3481c
Ray Kuo <ray20200720@gmail.com>
2024-01-30 17:37:07 +0800

    update EF6Example: use Code First Fluent API

--- Examples/EF6Example/Program.cs
+++ Examples/EF6Example/Program.cs
@@ -38,6 +38,11 @@ namespace EF6Example
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().Property(u => u.DisplayName).HasColumnName("display_name");
+        }
     }
 
     class Program
