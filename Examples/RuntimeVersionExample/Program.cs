72776e8e67cf9e475e0d8eabb337c6f7bf39d61c
Ray Kuo <ray20200720@gmail.com>
2024-01-30 16:41:30 +0800

    create RuntimeVersionExample

---
+++ Examples/RuntimeVersionExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace RuntimeVersionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(typeof(object).Assembly.Location);
+
+            Console.WriteLine(Environment.Version);
+
+            // .NET Framework 4.7.1 以上
+            Console.WriteLine($"Runtime: {RuntimeInformation.FrameworkDescription}");
+
+            Console.Read();
+        }
+    }
+}
