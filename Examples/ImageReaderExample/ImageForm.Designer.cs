1fe391a76544de5cb47452f4ab296fa4dd95d5e3
Ray Kuo <ray20200720@gmail.com>
2024-03-01 17:33:23 +0800

    create ImageReaderExample

---
+++ Examples/ImageReaderExample/ImageForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ImageReaderExample
+{
+    partial class ImageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.btnImageRotate = new System.Windows.Forms.Button();
+            this.btnImageCrop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(40, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(684, 330);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Location = new System.Drawing.Point(40, 381);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(157, 44);
+            this.btnLoadImage.TabIndex = 1;
+            this.btnLoadImage.Text = "Load Image";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
+            // 
+            // btnImageRotate
+            // 
+            this.btnImageRotate.Location = new System.Drawing.Point(219, 381);
+            this.btnImageRotate.Name = "btnImageRotate";
+            this.btnImageRotate.Size = new System.Drawing.Size(157, 44);
+            this.btnImageRotate.TabIndex = 2;
+            this.btnImageRotate.Text = "Image Rotate";
+            this.btnImageRotate.UseVisualStyleBackColor = true;
+            this.btnImageRotate.Click += new System.EventHandler(this.btnImageRotate_Click);
+            // 
+            // btnImageCrop
+            // 
+            this.btnImageCrop.Location = new System.Drawing.Point(405, 381);
+            this.btnImageCrop.Name = "btnImageCrop";
+            this.btnImageCrop.Size = new System.Drawing.Size(157, 44);
+            this.btnImageCrop.TabIndex = 3;
+            this.btnImageCrop.Text = "Image Crop";
+            this.btnImageCrop.UseVisualStyleBackColor = true;
+            this.btnImageCrop.Click += new System.EventHandler(this.btnImageCrop_Click);
+            // 
+            // ImageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnImageCrop);
+            this.Controls.Add(this.btnImageRotate);
+            this.Controls.Add(this.btnLoadImage);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "ImageForm";
+            this.Text = "ImageReaderExample";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.Button btnImageRotate;
+        private System.Windows.Forms.Button btnImageCrop;
+    }
+}
+
---
+++ Examples/ImageReaderExample/ImageForm.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageReaderExample
+{
+    public partial class ImageForm : Form
+    {
+        private int zoomLevel = 0;
+
+        public ImageForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "請選擇要開啟的圖片";
+            openFileDialog.Filter = "(*.jpg;*.bmp;*.png)|*.jpeg;*.jpg;*.bmp;*.png|All Files(*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+                Image image = Image.FromFile(fileName);
+                pictureBox1.Image = image;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+        }
+
+        private void btnImageRotate_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                pictureBox1.Refresh();
+            }
+        }
+
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                zoomLevel++;
+                if (zoomLevel > 4)
+                    zoomLevel = 4;
+            }
+            else if (e.Delta < 0)
+            {
+                zoomLevel--;
+                if (zoomLevel < -3)
+                    zoomLevel = -3;
+            }
+
+            zoomPicturebox();
+        }
+
+        private void zoomPicturebox()
+        {
+            switch (zoomLevel)
+            {
+                case -3:
+                    pictureBox1.Width -= 210;
+                    pictureBox1.Height -= 210;
+                    break;
+                case -2:
+                    pictureBox1.Width -= 155;
+                    pictureBox1.Height -= 155;
+                    break;
+                case -1:
+                    pictureBox1.Width -= 65;
+                    pictureBox1.Height -= 65;
+                    break;
+                case 0:
+                    pictureBox1.Size = new Size(850, 1100);
+                    break;
+                case 1:
+                    pictureBox1.Width += 75;
+                    pictureBox1.Height += 75;
+                    break;
+                case 2:
+                    pictureBox1.Width += 150;
+                    pictureBox1.Height += 150;
+                    break;
+                case 3:
+                    pictureBox1.Width += 175;
+                    pictureBox1.Height += 175;
+                    break;
+                case 4:
+                    pictureBox1.Width += 200;
+                    pictureBox1.Height += 200;
+                    break;
+            }
+
+            pictureBox1.Refresh(); // 刷新 PictureBox 控件，防止图像变得模糊
+        }
+
+        private void CropImage(Rectangle cropRegion)
+        {
+            // 创建一个新的 Bitmap 对象，大小与裁剪区域相同
+            Bitmap croppedImage = new Bitmap(cropRegion.Width, cropRegion.Height);
+
+            // 使用 Graphics 对象将原始图像的指定区域绘制到新的 Bitmap 中
+            using (Graphics graphics = Graphics.FromImage(croppedImage))
+            {
+                graphics.DrawImage(pictureBox1.Image, new Rectangle(0, 0, cropRegion.Width, cropRegion.Height), cropRegion, GraphicsUnit.Pixel);
+            }
+
+            // 将裁剪后的图像设置到 PictureBox 控件中
+            pictureBox1.Image = croppedImage;
+        }
+
+        private void btnImageCrop_Click(object sender, EventArgs e)
+        {
+            if(pictureBox1.Image != null)
+            {
+                CropImage(new Rectangle(0, 100, 1280, 145));
+            }
+        }
+    }
+}
