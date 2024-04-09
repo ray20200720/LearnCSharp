cccd31c2a0686e36fd60e6870cdc22c417fc3000
Ray Kuo <ray20200720@gmail.com>
2024-04-09 16:22:15 +0800

    Create Xml Example

---
+++ Examples/XmlExample/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XmlExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region example 1
+            
+            /*  
+             *  https://www.cnblogs.com/liweis/p/13714648.html
+             *  Load：支持xml路径、文件流、不支持XML文本；
+             *  LoadXml：支持XML文本，不支持xml路径。
+            */
+
+            XmlDataDocument xmlDocument1 = new XmlDataDocument();
+
+            //xmlDocument.LoadXml("D:\\people.xml"); //System.Xml.XmlException 在根層次的資料無效。 行 1，位置 1。 
+            string xml = "<?xml version=\"1.0\" encoding=\"utf-8\" ?><people><person><name>Ray</name><age>20</age></person><person><name>Yumiko</name><age>18</age></person></people>";
+            xmlDocument1.LoadXml(xml);
+            
+            Console.WriteLine(xmlDocument1.InnerText);
+
+
+            XmlDataDocument xmlDocument2 = new XmlDataDocument();
+
+            xmlDocument2.Load(@"D:\\Ray\\MyProjects\\LearnCSharp\\Examples\\XmlExample\\people.xml");
+            Console.WriteLine(xmlDocument2.InnerText);
+
+            #endregion
+
+            Console.ReadLine();
+
+            #region example 2
+            DataSet dataSet = new DataSet("MyDataSet");
+            DataTable dataTable = new DataTable("MyDataTable");
+
+            dataTable.Columns.Add("ID", typeof(int));
+            dataTable.Columns.Add("Name",  typeof(string));
+            dataTable.Rows.Add(1, "Ray");
+            dataTable.Rows.Add(2, "Yumiko");
+            dataSet.Tables.Add(dataTable);
+
+            XmlDataDocument xmlDataDocument = new XmlDataDocument(dataSet);
+
+            XmlElement rootElement = xmlDataDocument.DocumentElement;
+            Console.WriteLine("Root element name: " + rootElement.Name);
+
+            XmlNodeList rows = rootElement.GetElementsByTagName("MyDataTable");
+            
+            foreach (XmlNode row in rows)
+            {
+                //Console.WriteLine(row.InnerText);
+                Console.WriteLine(row.SelectSingleNode("ID").InnerText);
+                Console.WriteLine(row.SelectSingleNode("Name").InnerText);
+            }
+
+            dataTable.Rows[0]["Name"] = "Carol";
+
+            xmlDataDocument.Save(Console.Out);
+
+            Console.WriteLine();
+
+            #endregion
+
+            Console.ReadLine();
+
+            #region example 3
+            XmlDocument xmlDocument3 = new XmlDocument();
+            XmlNode elements = xmlDocument3.CreateElement("students");
+
+            XmlElement student1 = xmlDocument3.CreateElement("student");
+            XmlAttribute attribute1 = xmlDocument3.CreateAttribute("id");
+            attribute1.Value = "1";
+
+            student1.Attributes.Append(attribute1);
+            //element.SetAttribute("id", i.ToString());
+
+            XmlElement name = xmlDocument3.CreateElement("name");
+            name.InnerText = "小明";
+            student1.AppendChild(name);
+
+            XmlElement age = xmlDocument3.CreateElement("age");
+            age.InnerText = "18";
+            student1.AppendChild(age);
+
+            elements.AppendChild(student1);
+
+
+            XmlElement student2 = xmlDocument3.CreateElement("student");
+            XmlAttribute attribute2 = xmlDocument3.CreateAttribute("id");
+            attribute2.Value = "2";
+
+            student2.Attributes.Append(attribute2);
+            //element.SetAttribute("id", i.ToString());
+
+            XmlElement name2 = xmlDocument3.CreateElement("name");
+            name2.InnerText = "小紅";
+            student2.AppendChild(name2);
+
+            XmlElement age2 = xmlDocument3.CreateElement("age");
+            age2.InnerText = "17";
+            student2.AppendChild(age2);
+
+            elements.AppendChild(student2);
+
+            xmlDocument3.AppendChild(elements);
+
+            xmlDocument3.Save(Console.Out);
+
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
---
+++ Examples/XmlExample/README.md
@@ -0,0 +1,25 @@
+﻿在 C# 中，`XmlElement` 和 `XmlNode` 是用於處理 XML 文件的兩個重要類別，但它們之間存在一些差異。讓我們來看看這些差異：
+
+1. **`XmlNode`**：
+   - `XmlNode` 是一個抽象基類，表示 XML 文件中的節點（例如元素、屬性、文本等）。
+   - 所有 XML 節點的基類都是 `XmlNode`。
+   - `XmlNode` 具有以下常用屬性和方法：
+     - `Name`：節點的名稱。
+     - `Value`：節點的值（如果是文本節點）。
+     - `Attributes`：節點的屬性集合。
+     - `ChildNodes`：節點的子節點集合。
+     - `ParentNode`：節點的父節點。
+     - `AppendChild(XmlNode childNode)`：將子節點添加到節點。
+     - `SelectSingleNode(string xpath)`：根據 XPath 選擇一個節點。
+
+2. **`XmlElement`**：
+   - `XmlElement` 是 `XmlNode` 的一個具體子類，表示 XML 文件中的元素節點。
+   - 元素節點是 XML 文件的主要結構單元，具有開始標記、結束標記和內容。
+   - `XmlElement` 具有以下特定於元素的屬性和方法：
+     - `TagName`：元素的名稱。
+     - `InnerText`：元素的內容（包括所有子節點的文本）。
+     - `SetAttribute(string name, string value)`：設置元素的屬性。
+     - `GetAttribute(string name)`：獲取元素的屬性值。
+
+總之，`XmlNode` 是一個通用的節點類別，而 `XmlElement` 是用於表示元素節點的具體子類。如果您需要處理元素節點，請使用 `XmlElement`；
+如果您需要處理其他節點（例如文本、註釋、屬性等），則使用 `XmlNode`。📝🚀
