af3f4e3dc46e26b40c971179d67c3588997df7ad
Ray Kuo <ray20200720@gmail.com>
2024-05-28 16:15:44 +0800

    create RegexExample

---
+++ Examples/RegexExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RegexExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hello World";
+            string pattern = "Hello";
+            bool result = System.Text.RegularExpressions.Regex.IsMatch(input, pattern);
+            Console.WriteLine(result);
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
